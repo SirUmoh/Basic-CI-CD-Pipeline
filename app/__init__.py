eb89511829fe51c88bffc7643051d9fad176f833
SirUmoh <umoh.gvwc@gmail.com>
2024-04-30 19:47:26 -0400

    Adjusting for linter

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-# This is the package-level initialization for the 'app' package
+from .app import app  # Importing the Flask app to make it accessible at the package level
 
 # Optional: Print a message when the package is imported
 print("Initializing the 'app' package")
 
 # Importing the main Flask application from 'main.py'
-from .app import app  # Importing the Flask app to make it accessible at the package level
+
 
 __all__ = ['app']
