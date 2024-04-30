2910509cdf4b504096cab676af5c8d2f2c0dc53f
SirUmoh <umoh.gvwc@gmail.com>
2024-04-30 19:50:43 -0400

    Adjusting for linter

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 # Optionally, you can import all tests or specific test cases
-from .test_app import *  # Example import of all test cases from 'test_main.py'
+from .test_app import test_homepage, client  # Example import of all test cases from 'test_main.py'
 
 # You can define what gets imported with 'from tests import *'
-__all__ = ['test_app']
+__all__ = ['test_homepage, client']
